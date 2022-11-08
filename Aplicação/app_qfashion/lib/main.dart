e8dd1861d401d40f0eb764f054ca7dcefe98a392
Ruan Diego <Ruandgn123@gmail.com>
2022-11-07 21:19:33 -0300

    Tela principal e navegação entre telas

--- Aplicação/app_qfashion/lib/main.dart
+++ Aplicação/app_qfashion/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/base/base_screen.dart';
 
 Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,19 +11,27 @@ Future <void> main() async {
   );
 
   runApp(MyApp());
-  FirebaseFirestore.instance.collection('teste').add({'teste':'teste'});
+  //FirebaseFirestore.instance.collection('usuarios').add({'usuario':'Usuario'});
+  FirebaseFirestore.instance
+      .collection('usuarios')
+      .doc('XiuMe9Sf2vC0QHUQFnv9')
+      .snapshots()
+      .listen((doc) {
+    print(doc.data());
+  });
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App qFashion',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: BaseScreen(),
     );
   }
 }

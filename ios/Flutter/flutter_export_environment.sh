b8303d25de7b4cbe58349152f4001f02ae895c4d
Ruan Diego <Ruandgn123@gmail.com>
2022-12-07 03:12:05 -0300

    a

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\GitHub\qFashion\Aplicação\app_qfashion"
+export "FLUTTER_ROOT=C:\Users\vinic\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\vinic\StudioProjects\app_qfashion"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
